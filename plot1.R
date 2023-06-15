b9eed7509031fc742ed8fdd908f41a7899d489cf
Jyothismaria <jyohtismariajoseph@gmail.com>
2023-06-15 11:14:38 -0400

    clean version of plot1.R code

--- plot1.R
+++ plot1.R
@@ -1,12 +1,8 @@
 plot1<- function(){
-  df <- read.csv2(file = "household_power_consumption.txt", header = T, sep = ";")
-  df$Date =  as.factor(df$Date)
-  df$Date =  strptime(df$Date, format = "%d/%m/%Y")
-  df$Date <- as.Date(df$Date)
-  subDF<-df[df$Date >=as.Date(" 2007-02-01") & df$Date <=as.Date(" 2007-02-02") , ]
-  subDF$Datetime <- paste(as.character(subDF$Date), subDF$Time)
+  df <- read.csv2("household_power_consumption.txt", header=TRUE, sep=";")
+  subDF <- df[df$Date %in% c("1/2/2007","2/2/2007") ,]
   dev.cur()
-  dev.copy(png, "plot1.png")
+  dev.copy(png,"plot1.png", width = 480, height = 480, units = "px")
   hist(as.numeric(subDF$Global_active_power), xlab = "Global Active Power (kilowatts)", col = "red", main = "Global Active Power")
   dev.off()
 }
